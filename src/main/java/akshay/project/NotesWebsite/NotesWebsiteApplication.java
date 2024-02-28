5ead6ddb9b366b579d12f0b37b838778771915e3
AkshayCK06 <85177165+AkshayCK06@users.noreply.github.com>
2024-02-28 10:15:32 +0530

    inital commit

---
+++ src/main/java/akshay/project/NotesWebsite/NotesWebsiteApplication.java
@@ -0,0 +1,13 @@
+package akshay.project.NotesWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akshay/project/NotesWebsite/NotesWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package akshay.project.NotesWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
